a7cb83758f9563244221ae04b9d5010c75f2739e
lingqi1818@gmail.com <lingqi1818@gmail.com@871d599c-1fd8-738c-d367-e38a9c5f858f>
2013-05-25 09:14:01 +0000

    ci by chenke

---
+++ trunk/easy-restful/src/main/java/org/codeanywhere/easyRestful/base/servlet/Test.java
@@ -0,0 +1,14 @@
+package org.codeanywhere.easyRestful.base.servlet;
+
+import java.net.URLDecoder;
+
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(URLDecoder.decode("%3A%5B%7B"));
+    }
+
+}
